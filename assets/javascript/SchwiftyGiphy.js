804701b0aed348d8d4af22391caebc017ab095f2
Jeffrey Hinzmann <JAHinzmann@gmail.com>
2017-12-05 17:45:21 -0500

    Got additional help from Eric Hoppenworth

--- assets/javascript/SchwiftyGiphy.js
+++ assets/javascript/SchwiftyGiphy.js
@@ -1,10 +1,22 @@
 // API KEY: HGLcvaY72LbRGB4bwBWw0N4xLDivgaAj 
 
 // AJAX call
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=Rick+Morty&api_key=HGLcvaY72LbRGB4bwBWw0N4xLDivgaAj&limit=10"
 
 $.ajax({
     url:queryURL,
     method: 'GET'
-}).done(function(response {
+}).done(function(response) {
     console.log(response);
-})
+    
+    for (var i = 0; i < 10; i++){
+        var myURL = response.data[i].images.original.url
+        console.log(myURL);
+        var image = $("<img>")
+        image.attr("src", myURL);
+        $("#rickAndMortyPortal").append(image)
+    }
+
+
+
+});
